0aedd20efa0af84e984814533a97fe033812fc8e
alamminsalo <0dosages-remits@icloud.com>
2026-05-14 21:29:18 +0300

    set fallback backedn to wasm

--- src/lib/SLM.svelte
+++ src/lib/SLM.svelte
@@ -21,7 +21,7 @@
     } catch (e) {
       try {
         generator = await pipeline("text-generation", "LiquidAI/LFM2.5-1.2B-Instruct-ONNX", {
-          device: 'auto',
+          device: 'wasm',
           dtype: 'q4',
         }) as TextGenerationPipeline;
       } catch (e) {
