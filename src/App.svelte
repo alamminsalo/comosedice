255c5f362f7bcb1a2d54ce803f44698ae367954b
alamminsalo <0dosages-remits@icloud.com>
2026-05-15 21:20:14 +0300

    theme changes

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   import SLM from './lib/SLM.svelte'
 </script>
 
-<div class="min-h-screen bg-gray-900 flex flex-col justify-center items-center p-6 text-white">
+<div class="min-h-screen flex flex-col justify-center items-center p-6 text-white">
   <h1 class="text-5xl font-black tracking-tighter text-white md:text-7xl">
     ¿Cómo se <span class="text-orange-600">dice?</span>
   </h1>
